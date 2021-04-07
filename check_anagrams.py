9423e755293c83ce81ab86c3e71db6ef645853b5
Saipriyavadisina <saipriyavadisina@gmail.com>
2021-04-07 23:38:28 +0530

    solving the anagrams

---
+++ check_anagrams.py
@@ -0,0 +1,9 @@
+def check_anagrams(string1, string2):
+    # YOUR CODE HERE
+    return sorted(list(string1)) == sorted(list(string2))
+
+if __name__ == "__main__":
+    string1, string2 = input().split()
+    result = check_anagrams(string1, string2)
+    # YOUR CODE HERE
+    print(result)
