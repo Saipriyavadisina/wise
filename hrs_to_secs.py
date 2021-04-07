2caf1d13f756539f151d87e87021a52b09def601
Saipriyavadisina <saipriyavadisina@gmail.com>
2021-04-07 23:20:01 +0530

    converting hours to seconds

---
+++ hrs_to_secs.py
@@ -0,0 +1,10 @@
+def hrs_to_secs(hrs):
+    # YOUR CODE HERE
+    return hrs * 3600
+
+
+if __name__ == "__main__":
+    hrs = int(input())
+    secs = hrs_to_secs(hrs)
+    # YOUR CODE HERE
+    print(secs)
