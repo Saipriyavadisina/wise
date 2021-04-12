1411c6ddd23e27e91b73f612bc1b2ae7eeeb44b1
Saipriyavadisina <saipriyavadisina@gmail.com>
2021-04-12 23:45:43 +0530

    highly divisible triangular number

---
+++ hyighly_divisibly.py
@@ -0,0 +1,15 @@
+def tau(num) :
+    var n = num;
+    var i = 2;
+    var p = 1;
+    if (num === 1) return 1;
+    while (i * i <= n) :
+        var c = 1;
+        while (n % i === 0) :
+            n/= i;
+            c++;
+        i++;
+        p*= c;
+    if (n === num || n > 1)
+        p*= 1 + 1;
+    return p;
