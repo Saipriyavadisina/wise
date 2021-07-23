1001d615045c091d595b8e56c1985b614f14dbfe
Saipriyavadisina <saipriyavadisina@gmail.com>
2021-07-23 23:12:31 +0530

    sum of columns of two matrices

---
+++ sum_of_columns.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int m,n; 
+    printf("Enter the number of rows and column\n");
+    scanf("%d %d",&m,&n);  
+    int arr[m][n];  
+    printf("Enter the elements of the matrix\n");
+    for(int i=0;i<m;i++)    
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("\nElements in the matrix are \n");
+    for(int i=0;i<m;i++)    
+   {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nColumn Sum....\n");
+    for(int i=0;i<m;i++)
+    {
+        int csum=0;
+        for(int j=0;j<n;j++)
+        {
+            csum=csum+arr[j][i];
+        }
+        printf("\nSum of all the elements in column %d is %d\n",i,csum);
+    }
+    return 0;
+}
