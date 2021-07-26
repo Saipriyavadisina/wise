ea10af03035462ba398457ff80f5941253f48c23
Saipriyavadisina <saipriyavadisina@gmail.com>
2021-07-27 00:14:46 +0530

    printing alphabets in order

---
+++ alphabets.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("Alphabets from A - Z are: \n");
+    for(i = 65; i <= 90; i++)
+    {
+        printf("%c\n", i);
+    }
+    return 0;
+}
